0c2dc4b4329134d3295db3b2ae3fedce05633c76
Adrienne Bisaillon <adrienne.bisaillon@gmail.com>
2022-01-14 08:51:37 -0500

    Update contact form fields

--- index.js
+++ index.js
@@ -9,24 +9,24 @@
     const formElements = Array.from(form);
     formElements.map(input => (data[input.name] = input.value));
 
-    // Construct an HTTP request
+    // create an HTTP request
     const xhr = new XMLHttpRequest();
     xhr.open(form.method, form.action, true);
     xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
     xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
 
-    // Send the collected data as JSON
+    // send data as JSON
     xhr.send(JSON.stringify(data));
 
-    // Callback function
+    // callback function
     xhr.onloadend = response => {
       if (response.target.status === 200) {
-        // The form submission was successful
+        // form submission was successful
         form.reset();
-        formResponse.innerHTML = 'Thanks for the message. I’ll be in touch shortly.';
+        formResponse.innerHTML = 'Thanks for reaching out to me! I\'ll get back to you soon.';
       } else {
-        // The form submission failed
-        formResponse.innerHTML = 'Something went wrong';
+        // form submission failed
+        formResponse.innerHTML = 'Sorry, something went wrong.';
         console.error(JSON.parse(response.target.response).message);
       }
     };
